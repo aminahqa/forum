7319827d8d9b0991785c1ce81343f4b578fadd46
Aminah Qurratu <just2mywork@gmail.com>
2014-07-07 10:33:29 +0700

    delete semuanya

--- protected/views/kategori/_view.php
+++
@@ -1,9 +0,0 @@
-<?php
-/* @var $this KategoriController */
-/* @var $data Kategori */
-?>
-
-<div class="view">
-	<?php echo CHtml::link(CHtml::encode($data->kategori.'('.$data->jumlah.')'), array('thread/index', 'id'=>$data->id)); ?>
-	<br />
-</div>
